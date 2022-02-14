e544d7779bf040cb5783ba82092a388d8d3e063c
Avazbek <foziljonovavazbek0807@gmail.com>
2022-02-15 04:25:29 +0500

    start site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Profile from "./PortfolioContainer/Home/Profile";
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Profile/>
     </div>
   );
 }
 
 export default App;
+
+
+
