9b7e76c8ac459670f6d8d20aa32e056d018fd998
Avazbek <foziljonovavazbek0807@gmail.com>
2022-02-25 20:51:55 +0500

    start portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import "./Portfolio.css";
 
 function Portfolio() {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image"></div>
+          <h3>This is a portfolio item title</h3>
+          <a href="#!" target='_blank'>Github</a>
+          <a href="#!" target='_blank'>Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -9,13 +9,89 @@ function Services() {
       <h5>What I Offer</h5>
       <h2>Services</h2>
 
-      <div className="container container__services">
+      <div className="container services__container">
         <article className="service">
           <div className="service_head">
             <h3>UI/UX Design</h3>
           </div>
 
           <ul className="service_list">
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service_head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service_head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon"/>
+              <p>Lerom ipsum dolar sit amet consectuter elit.</p>
+            </li>
             <li>
               <BiCheck className="service_list-icon"/>
               <p>Lerom ipsum dolar sit amet consectuter elit.</p>
