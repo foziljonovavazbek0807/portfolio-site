d752bb4ed79775db320067fc9182ac03c5424ae9
Avazbek <foziljonovavazbek0807@gmail.com>
2022-02-27 23:39:51 +0500

    start footer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 import "./Contact.css";
+import {MdOutlineEmail} from "react-icons/md";
+import {FaTelegramPlane} from "react-icons/fa";
+import {FiInstagram} from "react-icons/fi";
 
 function Contact() {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+      <div className="contact_options">
+        <article className="contact_option">
+          <MdOutlineEmail className="contact_option-icon"/>
+          <h4>Email</h4>
+          <h5>foziljonovavazbek0807@gmail.com</h5>
+          <a href="#!" target="_blank">Send a message</a>
+        </article>
+        <article className="contact_option">
+          <FaTelegramPlane className="contact_option-icon"/>
+          <h4>Telegram</h4>
+          <h5>Prince</h5>
+          <a href="#!" target="_blank">Send a message</a>
+        </article>
+        <article className="contact_option">
+          <FiInstagram className="contact_option-icon"/>
+          <h4>Instagram</h4>
+          <h5>Prince</h5>
+          <a href="#!" target="_blank">Send a message</a>
+        </article>
+      </div>
+      <form action="#!">
+        <input type="text" name="name" placeholder="Your Full Name" required/>
+        <input type="email" name="email" placeholder="Your Email" required/>
+        <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+        <button className="btn btn-primary">Send Message</button>
+      </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import "./Footer.css";
+import {FaFacebookF} from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#!" className="footer_logo">Prince</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="#!"><FaFacebookF/></a>
+      </div>
+    </footer>
   )
 }
 
